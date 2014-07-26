8ee9986babfd498f239dc8bbb0f50bb8914897aa
Calvin Metcalf <calvin.metcalf@state.ma.us>
2014-07-26 18:55:33 -0400

    changes

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
 'use strict';
-var fs = require('fs');
 var assert = require('assert');
 var wordList = require('./index');
 
 it('should return list of words', function () {
-	assert(wordList.length > 0);
-	assert(fs.existsSync(wordList));
-	assert(fs.statSync(wordList).size > 1000);
+	assert(wordList.length > 1000);
 });
